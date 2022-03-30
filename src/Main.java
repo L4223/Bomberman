43e6abd4f89f3b75f86d097dbc698dafdf41abc2
jason01-95 <94526325+jason01-95@users.noreply.github.com>
2022-03-30 17:12:13 +0200

    main hinzugefügt

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings () {
+        size(200,300);
+    }
+
+    public void setup () {
+        background(0);
+    }
+
+    public void draw () {
+        background(255);
+    }
+}
